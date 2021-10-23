384edb97f2d1b7f3be5c82543efa555c4f2290a1
Alexei Yaskin <a.yaskin.stc@innopolis.university>
2021-10-23 22:18:36 +0700

    Change styles to use LESS, create animation transition for product tiles

--- homework_03_gulp-less-animation/gulpfile.js
+++ homework_03_gulp-less-animation/gulpfile.js
@@ -1,9 +1,46 @@
 const gulp = require('gulp');
 const less = require('gulp-less');
+const browserSync = require('browser-sync').create();
 
-gulp.task('less', function (done) {
+gulp.task('less', function (done) { // requires a callback to signal async completion
     gulp.src('./src/**/*.less')
         .pipe(less())
         .pipe(gulp.dest('./build/'));
     done();
 });
+
+gulp.task('html', function (done) {
+    gulp.src('./src/**/*.html')
+        .pipe(gulp.dest('./build/'));
+    done();
+});
+
+gulp.task('js', function (done) {
+    gulp.src('./src/**/*.js')
+        .pipe(gulp.dest('./build/'));
+    done();
+});
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: './build/'
+        },
+        port: 3000
+    });
+    gulp.watch('./build/**/*').on('change', browserSync.reload);
+});
+
+gulp.task('less:watch', function () {
+    gulp.watch('./src/**/*.less', gulp.series('less'));
+});
+
+gulp.task('html:watch', function () {
+    gulp.watch('./src/**/*.html', gulp.series('html'));
+});
+
+gulp.task('js:watch', function () {
+    gulp.watch('./src/**/*.js', gulp.series('js'));
+});
+
+gulp.task('default', gulp.parallel('less:watch', 'html:watch', 'js:watch', 'serve'));
