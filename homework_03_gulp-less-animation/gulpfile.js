64627ad69d59a9325945002ffebf10a705d48d7d
Alexei Yaskin <a.yaskin.stc@innopolis.university>
2021-10-28 22:10:57 +0700

    Port to scss, add PostCSS with cssnano to build distribution files

--- homework_03_gulp-less-animation/gulpfile.js
+++ homework_03_gulp-less-animation/gulpfile.js
@@ -1,10 +1,20 @@
 const gulp = require('gulp');
 const less = require('gulp-less');
+const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
 
 gulp.task('less', function (done) { // cb done() works quicker than return (5ms vs 50ms!)
     gulp.src('./src/**/*.less')
         .pipe(less())
+        .pipe(gulp.dest('./build/'));
+    done();
+});
+
+gulp.task('sass', function (done) {
+    gulp.src('./src/**/*.scss')
+        .pipe(sass())
         .pipe(gulp.dest('./build/'));
     done();
 });
@@ -27,7 +37,17 @@ gulp.task('serve', function() {
 
 gulp.task('build', function () {
     gulp.watch('./src/**/*.less', gulp.series('less'));
+    gulp.watch('./src/**/*.scss', gulp.series('sass'));
     gulp.watch('./src/**/*.{html,js}', gulp.series('copy'));
+});
+
+gulp.task('distr', function (done) {
+    gulp.src('./build/**/*.css')
+        .pipe(postcss([cssnano]))
+        .pipe(gulp.dest('./dist/'));
+    gulp.src('./build/**/*.{html,js}')
+        .pipe(gulp.dest('./dist/'));
+    done();
 });
 
 gulp.task('default', gulp.parallel('build', 'serve'));
